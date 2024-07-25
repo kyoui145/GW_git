06c94ff3481da41f7574eb6c14b69d5ee598db88
Nakamura <osaka.taro@exmanple.com>
2024-07-25 10:00:22 +0900

    中村　トップページ（ログイン）画面たたき台作成

---
+++ GroupWorkB/resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GroupWorkB</title>
+</head>
+<body>
+    <form action="" method="post">
+        ユーザ名：<input type="text" name="username"><br>
+        パスワード：<input type="text" name="password"><br>
+        <input type="submit" value="ログイン">
+        <!-- 時間があれば追加機能で作る -->
+        <!--<a href="">新規登録</a>-->
+        <!--<a href="">パスワード再設定</a>-->
+    </form>
+</body>
+</html>
