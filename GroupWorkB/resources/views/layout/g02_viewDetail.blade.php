00ba0978370238047d2fe99edcfe0379c296e526
小泉海 <koizumi.kai@example.com>
2024-07-26 10:30:56 +0900

    小泉　g02_viewDetailファイルのひな型作成

---
+++ GroupWorkB/resources/views/layout/g02_viewDetail.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>書籍詳細画面</title>
+</head>
+<body>
+    <h1>書籍詳細画面</h1>
+    <a href="/views/layout/g01_viewAll">戻る</a>
+</body>
+</html>
