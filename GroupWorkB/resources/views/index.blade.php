0444b74ba2b8552f5809c698bb94df7968c9a4fa
山本萌日香 <osaka.taro@example.com>
2024-07-26 14:03:29 +0900

    山本　ログイン画面のデザイン

--- GroupWorkB/resources/views/index.blade.php
+++ GroupWorkB/resources/views/index.blade.php
@@ -3,18 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+       body {
+            background-color: #FFFCF1;
+        }
+        .image-container {
+            display: flex;
+            justify-content: center; /* 左右中央揃え */
+            width: 100%;
+        }
+    </style>
     <title>GroupWorkB</title>
 </head>
 <body>
+    <div class="image-container">
+        <img src="img/books.webp" alt="本の画像">
+    </div>
     <!--ログインボタンを押すとログイン処理を実施し、成功すると書籍一覧画面に遷移する-->
-    <form action="/layout/login" method="post">
-        @csrf
-        ユーザ名：<input type="text" name="username"><br>
-        パスワード：<input type="text" name="password"><br>
-        <input type="submit" value="ログイン">
-        <!-- 時間があれば追加機能で作る -->
-        <!--<a href="">新規登録</a>-->
-        <!--<a href="">パスワード再設定</a>-->
-    </form>
+    <div style="width: 300px;margin:10px auto 0;padding: 20px;background-color: #ffffff;border: 1px solid #ccc;border-radius: 5px;">
+        <form action="/layout/login" method="post" style="font-size: 24px;text-align: center;">
+            @csrf
+            <p　style="text-align: center;">図書館管理システム B</p>
+            ユーザ名 ：<input type="text" name="username" style=" width:30%;padding: 10px;margin-bottom: 10px;border: 1px solid #ccc;border-radius: 4px;"><br>
+            パスワード：<input type="text" name="password" style=" width: 30%;padding: 10px;margin-bottom: 10px;border: 1px solid #ccc;border-radius: 4px;"><br>
+            <input type="submit" value="ログイン" style="width: 30%;padding: 10px;background-color: #808080;color: #ffffff;border: none;border-radius: 4px;cursor: pointer;">
+            <!-- 時間があれば追加機能で作る -->
+            <!--<a href="">新規登録</a>-->
+            <!--<a href="">パスワード再設定</a>-->
+        </form>
+    </div>  
 </body>
 </html>
--- GroupWorkB/resources/views/layout/g01_viewAll.blade.php
+++ GroupWorkB/resources/views/layout/g01_viewAll.blade.php
@@ -17,6 +17,11 @@
       integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
       crossorigin="anonymous"
      />
+     <style>
+       body {
+            background-color: #FFFCF1;
+        }
+    </style>
     <title>Document</title>
 </head>
 <body>
